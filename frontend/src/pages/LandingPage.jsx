2e3786ecdac2596d4354019404dacef6ba85355a
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2025-06-28 23:25:04 +0530

    Added footer

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import NavbarLanding from '../components/NavbarLanding'
 import { ButtonGreen } from '../components/ButtonGreen'
 import { ButtonGreenInverse } from '../components/ButtonGreenInverse'
 import {
-  IconArrowRight,
   IconLock,
-  IconKey
+  IconLockCheck,
+  IconDeviceMobileMessage,
+  IconBuildingSkyscraper,
+  IconKey,
+  IconBrandGithub,
+  IconBrandX,
+  IconBrandLinkedin
 } from '@tabler/icons-react';
-import { GiKey } from "react-icons/gi";
 import CardsCarousel from '../components/CardsCarousel';
-
+import {motion} from 'framer-motion'; 
 import { HiArrowRight } from "react-icons/hi2";
 
 const LandingPage = () => {
   return (
-    <div className="flex flex-col bg-[#fffffa] dark:bg-[#0a0a0a] items-center relative py-6 overflow-y-auto">
+    <div className="flex flex-col bg-[#fffffa] dark:bg-[#0a0a0a] items-center relative pt-6 overflow-y-auto">
       
       <NavbarLanding />
 
@@ -31,12 +36,296 @@ const LandingPage = () => {
       <HeroDesc3 />
       <CardsCarousel />
 
+      <HeroDesc4 />
+      <HeroDesc5 />
+
+      <Footer />
+
+
     </div>
   )
 }
 
 export default LandingPage
 
+
+ 
+
+const Footer=()=>{
+    const regions = [
+      {
+        imgSrc: "eu-flag.webp",
+        alt: "EU",
+        title: "Europe",
+        description: "something again to, something some",
+      },
+      {
+        imgSrc: "india-flag.webp",
+        alt: "India",
+        title: "India",
+        description: "Empowering innovation and tradition",
+      },
+      {
+        imgSrc: "usa-flag.webp",
+        alt: "USA",
+        title: "USA",
+        description: "Driving technology and progress",
+      },
+      {
+        imgSrc: "eu-flag.webp",
+        alt: "EU",
+        title: "Europe",
+        description: "Uniting cultures and economies",
+      },
+      {
+        imgSrc: "india-flag.webp",
+        alt: "India",
+        title: "India",
+        description: "Rich heritage, bright future",
+      },
+    ];
+
+
+    const fullstack = [
+      {
+        href: "https://github.com/kinshuk-dewari/abode",
+        label: "Abode",
+      },
+      {
+        href: "https://github.com/kinshuk-dewari/My-Wallet-MERN-Stack",
+        label: "My wallet",
+      },
+      {
+        href: "https://sai-agro.org/",
+        label: "Sai-agro",
+      },
+      {
+        href: "https://github.com/kinshuk-dewari/PRESENCE",
+        label: "Presence",
+      },
+      {
+        href: "https://github.com/kinshuk-dewari/Bookify",
+        label: "Bookify",
+      },
+    ];
+
+  const frontend = [
+    {
+      href: "https://kinshuk-dewari.netlify.app/",
+      label: "Portfolio website",
+    }
+  ];
+
+  const others = [
+    {
+      href: "https://github.com/kinshuk-dewari/Maze",
+      label: "Maze",
+    },
+    {
+      href: "https://github.com/kinshuk-dewari/Auto-Clicker",
+      label: "Auto-Clicker",
+    },
+    {
+      href: "https://github.com/kinshuk-dewari/Space_Invader",
+      label: "Space Invader",
+    },
+    {
+      href: "https://github.com/kinshuk-dewari/GHUUNJ-Sign-Language-Detection-ML-Project",
+      label: "Ghuunj",
+    },
+  ];
+
+
+  return (
+    <div className='w-full bg-neutral-100'>
+      <div className='max-w-6xl mx-auto py-24'>
+        <h1 className='font-bold text-6xl text-[#0a0a0a]'>Wise works nearly everywhere</h1>
+
+        <div className='grid grid-cols-5 pt-24'>
+          {regions.map((region, idx) => (
+            <div key={idx} className='space-y-2'>
+              <img
+                src={region.imgSrc}
+                alt={region.alt}
+                className="w-14 h-14 rounded-full object-cover"
+              />
+              <h3 className='text-neutral-700 text-2xl font-bold'>
+                {region.title}
+              </h3>
+              <p className='text-neutral-500 font-medium text-lg text-pretty'>
+                {region.description}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <div className='border-b-1 border-neutral-300 pt-24'></div>
+
+        <h1 className='text-base text-neutral-600 font-bold text-center py-4'>more project from me</h1>
+        <div className='grid grid-cols-4'>
+
+          <div className='flex flex-col'>
+            <h3 className='font-bold text-base text-neutral-700 pb-4'>Full-Stack</h3>
+            {fullstack.map((link) => (
+              <motion.a
+                key={link.href}
+                initial={{ y: 0 }}
+                whileHover={{ y: -1, transition: { duration: 0.2 } }}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-base underline text-neutral-500 hover:text-neutral-700 hover:cursor-pointer"
+              >
+                {link.label}
+              </motion.a>
+            ))}
+         </div>
+
+          <div className='flex flex-col'>
+            <h3 className='font-bold text-base text-neutral-700 pb-4'>Frontend</h3>
+            {frontend.map((link) => (
+              <motion.a
+                key={link.href}
+                initial={{ y: 0 }}
+                whileHover={{ y: -1, transition: { duration: 0.2 } }}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-base underline text-neutral-500 hover:text-neutral-700 hover:cursor-pointer"
+              >
+                {link.label}
+              </motion.a>
+            ))}
+          </div>
+
+          <div className='flex flex-col'>
+            <h3 className='font-bold text-base text-neutral-700 pb-4'>Others</h3>
+            {others.map((link) => (
+              <motion.a
+                key={link.href}
+                initial={{ y: 0 }}
+                whileHover={{ y: -1, transition: { duration: 0.2 } }}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-base underline text-neutral-500 hover:text-neutral-700 hover:cursor-pointer"
+              >
+                {link.label}
+              </motion.a>
+            ))}
+          </div>
+
+          <div className='flex flex-col'>
+            <div>
+              <h3 className='font-bold text-base text-neutral-700 pb-4'>Socials</h3>
+            </div>
+            <div className='flex items-start justify-start gap-4'>
+              <motion.a
+                initial={{ y: 0 }}
+                whileHover={{ y: -2, transition: { duration: 0.2 } }}
+                href="https://x.com/Kinshuk_Dewari"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-neutral-500 hover:text-neutral-700"
+              >
+                <IconBrandX className="h-8 w-8" />
+              </motion.a>
+
+              <motion.a
+                initial={{ y: 0 }}
+                whileHover={{ y: -2, transition: { duration: 0.2 } }}
+                href="https://linkedin.com/in/kinshuk-dewari"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-neutral-500 hover:text-neutral-700"
+              >
+                <IconBrandLinkedin className="h-8 w-8" />
+              </motion.a>
+
+              <motion.a
+                initial={{ y: 0 }}
+                whileHover={{ y: -2, transition: { duration: 0.2 } }}
+                href="https://github.com/kinshuk-dewari"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-neutral-500 hover:text-neutral-700"
+              >
+                <IconBrandGithub className="h-8 w-8" />
+              </motion.a>
+            </div>
+          </div>
+          
+          
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+const HeroDesc5 = () =>{
+  return (
+    <div className='space-y-8 py-24 bg-[#fffffa] dark:bg-[#0a0a0a] w-full'>
+      <div className='max-w-6xl space-y-8 mx-auto'>
+        <div className='flex items-center justify-between'>
+          <div className='space-y-8'>
+            <h1 className='text-6xl font-bold text-[#0a0a0a] dark:text-[#fffffa]'>Disapponit thieves</h1>
+            <p className='text-neutral-600 text-xl text-pretty max-w-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur inventore eum et culpa at illo aperiam a error quibusdam molestiae .</p>
+            <ButtonGreen title='How we keep you money safe'/>
+          </div>
+          <img src="lock.png" className='max-w-[40%]' alt="" />
+        </div>
+        <div className='grid grid-cols-4'>
+          <div className="space-y-6">
+            <div className="flex items-center justify-center bg-neutral-200 dark:bg-neutral-500 rounded-full h-16 w-16">
+              <IconLockCheck className="w-10 h-10 text-[#0a0a0a]" />
+            </div>
+            <p className="text-xl text-neutral-700 text-pretty">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Something here.
+            </p>
+          </div>
+
+          <div className="space-y-6">
+            <div className="flex items-center justify-center bg-neutral-200 dark:bg-neutral-500 rounded-full h-16 w-16">
+              <IconDeviceMobileMessage className="w-10 h-10 text-[#0a0a0a]" />
+            </div>
+            <p className="text-xl text-neutral-700 text-pretty">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Something here.
+            </p>
+          </div>
+
+          <div className="space-y-6">
+            <div className="flex items-center justify-center bg-neutral-200 dark:bg-neutral-500 rounded-full h-16 w-16">
+              <IconBuildingSkyscraper className="w-10 h-10 text-[#0a0a0a]" />
+            </div>
+            <p className="text-xl text-neutral-700 text-pretty">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Something here.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+const HeroDesc4=()=>{
+  return (
+    <div className='space-y-8 py-24 bg-[#1c3108] w-full'>
+      <div className='max-w-6xl space-y-8 uppercase font-extrabold mx-auto'>
+        <h1 className='text-8xl text-[#aee780] text-pretty'>meet money without borders</h1>
+        <div className='grid grid-cols-2'>
+          <p className='col-start-2 text-[#fffffa] text-3xl font-medium text-pretty normal-case'>Our dream is for people to live and work anywhere seamlesslly. That means money without borders: moving it instantantly, transparentlt, convinently, and - eventually - for free.</p>
+        </div>
+        <img src="pocket.png" className='w-full pt-4' alt="Pocket" />
+        <div className='flex  items-start pt-8 justify-between'>
+          <p className='text-[#fffffa] text-2xl w-1/2 font-light text-pretty normal-case'>The Wise account is the universal way for you to manage money internationally. It's made for the world. And it's built to save your money and time, so you can do more of the things you love. </p>
+          <ButtonGreen title='Learn about our mission'/>
+        </div>
+      </div>
+    </div>
+  )
+}
 const HeroSection=()=>{
   return (
     <div className='space-y-8 pt-42'>
